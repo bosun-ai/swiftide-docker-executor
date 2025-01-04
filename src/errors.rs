5a4e54dc4d1473a74165461844e443040088f501
Timon Vonk <mail@timonv.nl>
2025-01-04 13:11:41 +0100

    feat: Initial commit

---
+++ src/errors.rs
@@ -0,0 +1,36 @@
+use std::{convert::Infallible, path::StripPrefixError};
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DockerExecutorError {
+    #[error("error from bollard")]
+    Bollard(#[from] bollard::errors::Error),
+
+    #[error("error building context for image")]
+    Context(#[from] ContextError),
+}
+
+#[derive(Error, Debug)]
+pub enum ContextError {
+    #[error("error iterating over directory")]
+    DirWalker(#[from] ignore::Error),
+
+    #[error("error reading file")]
+    Io(#[from] std::io::Error),
+
+    #[error("failed to convert to relative path")]
+    RelativePath(#[from] StripPrefixError),
+}
+
+impl From<Infallible> for DockerExecutorError {
+    fn from(_: Infallible) -> Self {
+        unreachable!()
+    }
+}
+
+impl From<Infallible> for ContextError {
+    fn from(_: Infallible) -> Self {
+        unreachable!()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod docker_tool_executor;
+mod errors;
+
+pub use docker_tool_executor::*;
+pub use errors::*;
